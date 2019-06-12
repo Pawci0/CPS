6c3b65cfdfa09fee189211398d0cd2683e316166
Krzysztof Barden <wiedzmenpl@gmail.com>
2019-06-12 22:48:38 +0200

    fix mess

---
+++ Visualization/Utils/ViewUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Lib;
+using LiveCharts.Defaults;
+
+namespace Visualization
+{
+    public class ViewUtils
+    {
+        public static IEnumerable<ObservablePoint> ToValues(RealSignal signal)
+        {
+            var result = new List<ObservablePoint>();
+            foreach (var (x, y) in signal.ToDrawGraph()) result.Add(new ObservablePoint(x, y));
+
+            return result;
+        }
+
+        public static IEnumerable<ObservablePoint> ToValues(List<(double x, double y)> list)
+        {
+            var result = new List<ObservablePoint>();
+            foreach (var (x, y) in list) result.Add(new ObservablePoint(x, y));
+
+            return result;
+        }
+    }
+}
